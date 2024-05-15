ce02e05af642428c4acf169cc4e6d13a59c4c022
Jay-Karia <jay.sanjay.karia@gmail.com>
2024-05-15 12:07:39 +0530

    added card back image

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,7 +7,8 @@ import { ImHeart } from 'react-icons/im'
 import { ImClubs } from 'react-icons/im'
 import { ImSpades } from 'react-icons/im'
 import { ImDiamonds } from 'react-icons/im'
-import { FaQuestion } from "react-icons/fa";
+import { FaQuestion } from 'react-icons/fa'
+import Image from 'next/image'
 
 export default function Card({ card }) {
   const suitMap = {
@@ -19,16 +20,30 @@ export default function Card({ card }) {
 
   return (
     <div className={'card-container flex flex-col'}>
-      <div className={'p-2 w-max h-max absolute'}>
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          {card.face}
-        </h1>
-      </div>
-      <div className={'p-10 w-full h-full flex justify-center'}>
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          {card.suit ? suitMap[card.suit] : <FaQuestion color={"#ff0000"} />}
-        </h1>
-      </div>
+      {card.face ? (
+        <>
+          <div className={'p-2 w-max h-max absolute'}>
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+              {card.face}
+            </h1>
+          </div>
+          <div className={'p-10 w-full h-full flex justify-center'}>
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+              {suitMap[card.suit]}
+            </h1>
+          </div>
+        </>
+      ) : (
+        <div className="h-full w-full">
+          <Image
+            src="/backImage.png"
+            alt="Image"
+            height={100}
+            width={100}
+            className="h-full w-full"
+          />
+        </div>
+      )}
     </div>
   )
 }
